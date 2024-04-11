98935854f415c3f8983acaf9c8d2f91216e3e321
Bodie <danadevops@gmail.com>
2024-04-11 11:21:22 -0400

    hi

--- 11-final-project/web/index.php
+++ 11-final-project/web/index.php
@@ -11,19 +11,49 @@
         <h1>Random Image Generator</h1>
         <?php
             $imagesDir = 'images/';
-            // $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-            $images=scandir($imagesDir);
+            $images = array_diff(scandir($imagesDir), array('..', '.'));
             $randomImage = $images[array_rand($images)]; 
-            print_r($randomImage);
         ?>
         <img id="randomImage" src="images/<?php echo $randomImage; ?>" alt="Random Image">
         <form action="index.php" method="post">
             <button type="submit" name="generateButton">Generate Random Image</button>
+            <input type="hidden" name="currentImage" value="<?php echo $randomImage; ?>">
+            <button type="submit" name="thumbsUpButton">Thumbs Up</button>
+            <button type="submit" name="thumbsDownButton">Thumbs Down</button>
         </form>
-    </div>
+        <?php
+            // Load votes from file
+            $votes = [];
+            if (file_exists("votes.txt")) {
+                $votes = unserialize(file_get_contents("votes.txt"));
+            }
 
-    <?php
-      
-    ?>
+            // Get vote counts for the current image
+            $currentImage = $randomImage;
+            $thumbsUpCount = isset($votes[$currentImage]['thumbsUp']) ? $votes[$currentImage]['thumbsUp'] : 0;
+            $thumbsDownCount = isset($votes[$currentImage]['thumbsDown']) ? $votes[$currentImage]['thumbsDown'] : 0;
+
+            // Display vote counts
+            echo "<p>Thumbs Up: $thumbsUpCount</p>";
+            echo "<p>Thumbs Down: $thumbsDownCount</p>";
+
+            // Update votes
+            if(isset($_POST['thumbsUpButton']) || isset($_POST['thumbsDownButton'])) {
+                $currentImage = $_POST['currentImage'];
+                if(isset($_POST['thumbsUpButton'])) {
+                    if(!isset($votes[$currentImage]['thumbsUp'])) {
+                        $votes[$currentImage]['thumbsUp'] = 0;
+                    }
+                    $votes[$currentImage]['thumbsUp']++;
+                } elseif(isset($_POST['thumbsDownButton'])) {
+                    if(!isset($votes[$currentImage]['thumbsDown'])) {
+                        $votes[$currentImage]['thumbsDown'] = 0;
+                    }
+                    $votes[$currentImage]['thumbsDown']++;
+                }
+                file_put_contents("votes.txt", serialize($votes));
+            }
+        ?>
+    </div>
 </body>
 </html>
